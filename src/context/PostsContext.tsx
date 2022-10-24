eacc7e6f7f378965abdc7f6f853eecfcfd81c0bb
DBorisov <dm1.br@yandex.com>
2022-10-24 23:34:40 +0300

    posts context reworked, deleted usePOsts hook, so it does not needed anymore

--- src/context/PostsContext.tsx
+++ src/context/PostsContext.tsx
@@ -1,19 +1,23 @@
+import axios from 'axios';
 import React, {
   createContext,
   Dispatch,
   SetStateAction,
   useState,
 } from 'react';
-import { IPost } from '../data/types';
-import { usePosts } from '../hooks/usePosts';
+import { INewPost, IPost } from '../data/types';
 
 interface IPostsContext {
   posts: IPost[];
   setPosts: Dispatch<SetStateAction<IPost[]>>;
   choosedPostID: number;
   setChoosedPostID: Dispatch<SetStateAction<number>>;
-  loading: boolean;
-  error: string;
+  exactPost: IPost;
+  getAllPosts: () => Promise<void>;
+  addPost: (post: INewPost) => Promise<void>;
+  deletePost: () => Promise<void>;
+  editPost: (post: IPost | undefined) => Promise<void>;
+  getExactPost: () => Promise<void>;
 }
 
 interface PostsStateProps {
@@ -22,6 +26,12 @@ interface PostsStateProps {
 
 export const PostsContext = createContext<IPostsContext>({
   posts: [],
+  exactPost: {
+    id: 0,
+    body: '',
+    title: '',
+    userId: 0,
+  },
   setPosts: () => {
     return;
   },
@@ -29,13 +39,75 @@ export const PostsContext = createContext<IPostsContext>({
   setChoosedPostID: () => {
     return;
   },
-  loading: false,
-  error: '',
+  getAllPosts: async () => {
+    return;
+  },
+  addPost: async () => {
+    return;
+  },
+  deletePost: async () => {
+    return;
+  },
+  editPost: async () => {
+    return;
+  },
+  getExactPost: async () => {
+    return;
+  },
 });
 
 const PostsState = ({ children }: PostsStateProps) => {
   const [choosedPostID, setChoosedPostID] = useState<number>(0);
-  const { posts, setPosts, loading, error } = usePosts();
+  const [exactPost, setExactPost] = useState<IPost>({
+    id: 0,
+    body: '',
+    title: '',
+    userId: 0,
+  });
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  const getAllPosts = async () => {
+    const response = await axios.get<IPost[]>(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    console.log('fetching all posts');
+
+    setPosts(response.data);
+  };
+
+  const addPost = async (post: INewPost) => {
+    const response = await axios.post<IPost>(
+      'https://jsonplaceholder.typicode.com/posts',
+      post
+    );
+
+    setPosts([...posts, response.data]);
+  };
+
+  const deletePost = async () => {
+    await axios.delete(
+      `https://jsonplaceholder.typicode.com/posts/${choosedPostID}`
+    );
+    setPosts([...posts].filter((post) => choosedPostID !== post.id));
+  };
+
+  const editPost = async (post: IPost | undefined) => {
+    const filteredPosts = posts.filter((post) => choosedPostID !== post.id);
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/posts/${choosedPostID}`,
+      post
+    );
+    const editedPost = response.data;
+
+    setPosts([...filteredPosts, editedPost]);
+  };
+
+  const getExactPost = async () => {
+    const response = await axios.get<IPost>(
+      `https://jsonplaceholder.typicode.com/posts/${choosedPostID}`
+    );
+    setExactPost(response.data);
+  };
 
   return (
     <PostsContext.Provider
@@ -44,8 +116,12 @@ const PostsState = ({ children }: PostsStateProps) => {
         setPosts,
         choosedPostID,
         setChoosedPostID,
-        loading,
-        error,
+        exactPost,
+        getExactPost,
+        getAllPosts,
+        addPost,
+        deletePost,
+        editPost,
       }}
     >
       {children}
